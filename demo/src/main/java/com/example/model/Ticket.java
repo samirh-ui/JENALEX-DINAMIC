9b7a1b915ea5569358ad5ff9584185c51274c832
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-04-30 14:19:41 -0500

    creando clase ticket con sus metodos y atributos

---
+++ demo/src/main/java/com/example/model/Ticket.java
@@ -0,0 +1,75 @@
+package com.example.model;
+
+import java.util.Date;
+
+public class Ticket {
+
+    private int id;
+    private String descripcion;
+    private String estado;
+    private String prioridad;
+    private int idUsuario;
+    private int idTecnico;
+    private Date fecha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdTecnico() {
+        return idTecnico;
+    }
+
+    public void setIdTecnico(int idTecnico) {
+        this.idTecnico = idTecnico;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    
+
+
+    
+}
